ddb951f16d3e602cfecdfb30281ff8fc7b98d285
Alper Şallı <alpersallidgb@gmail.com>
2023-09-04 13:14:24 +0300

    sending json data

--- esp32_code/esp32_code.ino
+++ esp32_code/esp32_code.ino
@@ -1,64 +1,78 @@
-/** Example of continuous scanning for BLE advertisements.
- * This example will scan forever while consuming as few resources as possible
- * and report all advertisments on the serial monitor.
- *
- * Created: on January 31 2021
- *      Author: H2zero
- *
- */
-
 #include "NimBLEDevice.h"
+#include <ArduinoJson.h>
+#include <WiFi.h> // Include the Wi-Fi library
+#include <HTTPClient.h>
+
 
 NimBLEScan* pBLEScan;
 
-class MyAdvertisedDeviceCallbacks: public NimBLEAdvertisedDeviceCallbacks {
+
+const char* ssid = "Zyxel_4A61";
+const char* password = "QXLYLP83ML";
+const char* apiUrl = "20.62.111.133:5000/api/receive_data";
+
+
+
+class MyAdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
     void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
-      if (advertisedDevice->haveName() && 
-          advertisedDevice->getName() == "OMIYA-C39-HW" &&
-          advertisedDevice->getAddress().equals(NimBLEAddress("0c:95:41:00:00:23"))) {
-            Serial.println("Raw Data:");
-            const uint8_t* payload = advertisedDevice->getPayload();
-            size_t payloadLength = advertisedDevice->getPayloadLength();
-            for (size_t i = 0; i < payloadLength; i++) {
-                Serial.printf("%02X ", payload[i]);
-            }
-          Serial.println();
-        }
+       /* if (advertisedDevice->haveName() &&
+            advertisedDevice->getName() == "OMIYA-C39-HW" &&
+            advertisedDevice->getAddress().equals(NimBLEAddress("0c:95:41:00:00:23"))) { */
+            
+            // Create a JSON object to store your data
+            StaticJsonDocument<200> jsonDoc;
+            jsonDoc["device_name"] =  "OMIYA-C39-HW"; //advertisedDevice->getName();
+            jsonDoc["device_address"] = "0c:95:41:00:00:23"; //advertisedDevice->getAddress().toString();
 
+            // Convert JSON to a string
+            String jsonData;
+            serializeJson(jsonDoc, jsonData);
 
+            // Send the data to your Flask API
+            sendToFlaskAPI(jsonData);
+        //}
     }
+
+    void sendToFlaskAPI(const String& data) {
+    HTTPClient http;
+    http.begin(apiUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    int httpResponseCode = http.POST(data);
+    if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+    } else {
+        Serial.print("Error sending data. HTTP error code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(http.errorToString(httpResponseCode).c_str());
+    }
+
+    http.end();
+}
 };
 
+
 void setup() {
+
+  Serial.begin(115200);
+  Serial.println("Connecting to Wi-Fi...");
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to Wi-Fi...");
+  }
+
+  Serial.println("Connected to Wi-Fi");
+
   Serial.begin(115200);
   Serial.println("Scanning...");
 
-/** *Optional* Sets the filtering mode used by the scanner in the BLE controller.
- *
- *  Can be one of:
- *  CONFIG_BTDM_SCAN_DUPL_TYPE_DEVICE (0) (default)
- *  Filter by device address only, advertisements from the same address will be reported only once.
- *
- *  CONFIG_BTDM_SCAN_DUPL_TYPE_DATA (1)
- *  Filter by data only, advertisements with the same data will only be reported once,
- *  even from different addresses.
- *
- *  CONFIG_BTDM_SCAN_DUPL_TYPE_DATA_DEVICE (2)
- *  Filter by address and data, advertisements from the same address will be reported only once,
- *  except if the data in the advertisement has changed, then it will be reported again.
- *
- *  Can only be used BEFORE calling NimBLEDevice::init.
-*/
   NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DEVICE);
 
-/** *Optional* Sets the scan filter cache size in the BLE controller.
- *  When the number of duplicate advertisements seen by the controller
- *  reaches this value it will clear the cache and start reporting previously
- *  seen devices. The larger this number, the longer time between repeated
- *  device reports. Range 10 - 1000. (default 20)
- *
- *  Can only be used BEFORE calling NimBLEDevice::init.
- */
   NimBLEDevice::setScanDuplicateCacheSize(200);
 
   NimBLEDevice::init("");
@@ -71,6 +85,67 @@ void setup() {
   pBLEScan->setWindow(37);  // How long to scan during the interval; in milliseconds.
   pBLEScan->setMaxResults(0); // do not store the scan results, use callback only.
 }
+
+/*
+void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
+    // Regardless of the device, always create and send the JSON data
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["device_name"] = advertisedDevice->getName();
+    jsonDoc["mac_address"] = advertisedDevice->getAddress().toString();
+    jsonDoc["raw_data"] = payloadToString(advertisedDevice->getPayload(), advertisedDevice->getPayloadLength());
+    
+    // Serialize the JSON object to a string
+    String jsonString;
+    serializeJson(jsonDoc, jsonString);
+    
+    // Send JSON data to the server
+    sendJsonToServer(jsonString);
+
+    if (advertisedDevice->haveName() && 
+        advertisedDevice->getName() == "OMIYA-C39-HW" &&
+        advertisedDevice->getAddress().equals(NimBLEAddress("0c:95:41:00:00:23"))) {
+            Serial.println("Raw Data:");
+            const uint8_t* payload = advertisedDevice->getPayload();
+            size_t payloadLength = advertisedDevice->getPayloadLength();
+            for (size_t i = 0; i < payloadLength; i++) {
+                Serial.printf("%02X ", payload[i]);
+            }
+            Serial.println();
+    }
+    
+}
+*/
+
+
+String payloadToString(const uint8_t* payload, size_t length) {
+    String result = "";
+    for (size_t i = 0; i < length; i++) {
+        char hex[3];
+        sprintf(hex, "%02X", payload[i]);
+        result += hex;
+    }
+    return result;
+}
+
+/*
+void sendJsonToServer(String jsonString) {
+    HTTPClient http;
+    http.begin("http://20.62.111.133/receive_data");
+    http.addHeader("Content-Type", "application/json");
+
+    int httpResponseCode = http.POST(jsonString);
+
+    if (httpResponseCode == 200) {
+        Serial.println("HTTP Response code: 200 - Data sent successfully");
+    } else {
+        Serial.print("HTTP Error: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();
+}
+*/
+
 
 void loop() {
   // If an error occurs that stops the scan, it will be restarted here.
