12134e8400c1aa7293197a5188c1d318d1a7eb32
Alper Şallı <alpersallidgb@gmail.com>
2023-09-07 12:56:48 +0300

    App

---
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.as.healthcaredeu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    Button homeScreenCreateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+}
--- healthcareDEU/app/src/main/java/com/as/healthcaredeu/MainActivity.java
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/MainActivity.java
@@ -2,7 +2,9 @@ package com.as.healthcaredeu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        homeScreenCreateButton = findViewById(R.id.createAccButton);
+        homeScreenCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
