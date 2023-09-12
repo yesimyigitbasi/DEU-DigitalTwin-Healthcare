a03db6a5b745f41dd00b327f619c73327fa48085
Alper Şallı <alpersallidgb@gmail.com>
2023-09-12 14:23:17 +0300

    notifications page updated

--- healthcareDEU/app/src/main/java/com/as/healthcaredeu/NotificationActivity.java
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/NotificationActivity.java
@@ -1,15 +1,57 @@
 package com.as.healthcaredeu;
 
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationActivity extends AppCompatActivity {
 
+    ImageView statisticsButton;
+    ImageView notificationsButton;
+    ImageView settingsButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications);
+
+        statisticsButton = findViewById(R.id.statisticsButton3);
+        notificationsButton = findViewById(R.id.notificationButton3);
+        settingsButton = findViewById(R.id.settingsButton3);
+
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationActivity.this,GraphActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        notificationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationActivity.this,NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
