43d5e2ade067148c9ede4510d5f7c2fabfeefe96
yesimyigitbasi <yesimyigitbasi@users.noreply.github.com>
2023-09-12 11:30:16 +0300

    activity class

--- healthcareDEU/app/src/main/java/com/as/healthcaredeu/EditGoalsActivity.java
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/EditGoalsActivity.java
@@ -1,4 +1,16 @@
 package com.as.healthcaredeu;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class EditGoalsActivity {
+    EditText weightGoal;
+    EditText stepCountGoal;
+    Button save;
+    Button cancel;
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+    }
 }
--- healthcareDEU/app/src/main/java/com/as/healthcaredeu/EditPersonalInfoActivity.java
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/EditPersonalInfoActivity.java
@@ -1,4 +1,34 @@
 package com.as.healthcaredeu;
 
-public class EditPersonalInfoActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditPersonalInfoActivity extends AppCompatActivity {
+    EditText editAge;
+    Spinner genderDropdown;
+    Button save;
+    Button cancel;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_personal_info);
+        editAge = findViewById(R.id.editTextAge);
+        Spinner genderDropdown = findViewById(R.id.spinnerGender);
+        String[] items = new String[]{"Female", "Male", "Prefer not to say"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        genderDropdown.setAdapter(adapter);
+        save = findViewById(R.id.saveBtn);
+        cancel = findViewById(R.id.cancelBtn);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
