b56eeb1f20252529c69f098fca2f74e6e1ea59d3
yesimyigitbasi <yesimyigitbasi@users.noreply.github.com>
2023-09-13 12:03:08 +0300

    login activity

--- healthcareDEU/app/src/main/java/com/as/healthcaredeu/loginActivity.java
+++ healthcareDEU/app/src/main/java/com/as/healthcaredeu/loginActivity.java
@@ -1,68 +1,58 @@
 package com.as.healthcaredeu;
 
-import android.annotation.SuppressLint;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class loginActivity extends AppCompatActivity {
+    private static String loginUrl = "http://20.62.111.133:80/api/login";
 
-    EditText usernameLogin;
-    EditText passwordLogin;
-    Button signInButton;
-    private static String url = "http://20.62.111.133:80/api/login";//****Put your  URL here******
-    private String POST = "POST";
-    private String GET = "GET";
+    Button loginButton;
+    EditText usernameText;
+    EditText passwordText;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
-        usernameLogin = findViewById(R.id.usernameLogin);
-        passwordLogin = findViewById(R.id.passwordLogin);
-        signInButton = findViewById(R.id.signInButton);
 
+        loginButton = findViewById(R.id.signInButton);
+        usernameText = findViewById(R.id.usernameLogin);
+        passwordText = findViewById(R.id.passwordLogin);
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String accUsername = (usernameLogin.getText().toString());
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
 
-                String accPassword = (passwordLogin.getText().toString());
-                //if(accUsername == "alper" && accPassword == "123"){
-                    Intent intent = new Intent(loginActivity.this,GraphActivity.class);
-                    startActivity(intent);
-               // }
-/*                MyVolleyRequest.getRequest(getApplicationContext(), url, new MyVolleyRequest.VolleyCallback() {
+                JSONObject loginData = new JSONObject();
+                try {
+                    loginData.put("username", username);
+                    loginData.put("password", password);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                MyVolleyRequest.postRequest(getApplicationContext(), loginUrl, loginData, new MyVolleyRequest.VolleyCallback() {
                     @Override
                     public void onSuccess(String result) {
-                        // Handle a successful response here
-                        // The 'result' parameter contains the response data as a string
+                        // Handle successful login, e.g., store user session or navigate to HomeScreenActivity
+                        Intent intent = new Intent(loginActivity.this, GraphActivity.class);
+                        startActivity(intent);
                     }
 
                     @Override
                     public void onError(String error) {
-                        // Handle an error here
-                        // For example, display an error message to the user
-                        Toast.makeText(getApplicationContext(), "Error: " + error, Toast.LENGTH_SHORT).show();
+                        // Handle login error, e.g., show an error message to the user
                     }
-                });*/
-
-
-
-
+                });
             }
         });
-
     }
 }
-
